5c688a1f63fbba127bf6558f48677da855665bb1
LEARN Academy <learnacademy@LEARNs-MacBook-Air-3.local>
2022-05-31 14:59:19 -0700

    started working on updating boxes on click

--- README.md
+++ README.md
@@ -6,9 +6,12 @@ This project will challenge your knowledge of React but should also be an exerci
 
 ### 📚 User Stories
 - As a user, I can see a 3x3 grid game board on the page.
--branch grid-creation
+- branch grid-creation
 create a section- div tag use flex box to easily organize the grid and style it. Use div to hold the grid and make a map to create an boxes that hold a value in the array.
 - As a user, I can click on a square to mark it.
+- branch mark-square
+- create an even that onClick changes the square clicked to an emoji.  
+
 - As a user, my partner can click on a square after me and see their mark.
 - As a user, I can't click on a square that has already been marked.
 - As a user, when either my partner or I win the game (3 squares in a row: horizontally, vertically, or diagonally) I can see a notice telling me which player won.
--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      squares: [null, null, null, null, null, null, null, null, null]
+      squares: [null, null, null, null, null, null, null, null, null],
+      user1: null
     }
   }
+  componentDidMount(){
+
+  }
+  handleGamePlay = (index) => {
+    const { squares } = this.state
+    if(index === user1){
+      squares[index] = "🎃"
+      this.setState({squares: squares})
+    }
+
+
 
   render() {
     return(
@@ -16,13 +28,16 @@ class App extends Component {
         <h1>Tic Tac Toe</h1>
         <div className= "gameBoard" >
           {this.state.squares.map((value, index)=>{
-            return <Square value={value} key={index} index = {index}/>
-          })}
-
-
+            return(
+              <Square
+            value={value}
+            key={index}
+            index={index}
+            handleGameplay={this.handleGameplay}
+            />
+          )
+        })}
         </div>
-        
-        
       </>
     )
   }
